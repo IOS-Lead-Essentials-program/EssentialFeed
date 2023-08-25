2963327db29ef05652e1eb214aab67910bac1097
neddyKG <gonzalezneddy@gmail.com>
2023-08-25 10:17:37 -0400

    Give a url instead of hardcoding it

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,13 +10,15 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL , client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -36,7 +38,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-given-url.com")!
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
@@ -47,9 +50,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
          Dependency injection: constructor, property or method injection.
          But there's no need for a singleton, cause there could be more than one API client.
          */
-        let sut = RemoteFeedLoader(client: client)
+        let url = URL(string: "https://a-given-url.com")!
+        let sut = RemoteFeedLoader(url: url, client: client)
         sut.load()
          
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
