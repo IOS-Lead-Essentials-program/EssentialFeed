42a8cc8a07a6f7509461adbc26bd105cc4c66973
neddyKG <gonzalezneddy@gmail.com>
2023-08-25 07:27:05 -0400

    Create FeedItem module

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Neddy Ksenia Gonzalez Siles on 25/8/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
