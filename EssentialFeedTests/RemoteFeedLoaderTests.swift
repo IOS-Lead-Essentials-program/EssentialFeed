7096960e1ada0d93e08a24a30cee094c9e7a94d8
neddyKG <gonzalezneddy@gmail.com>
2023-08-25 11:36:23 -0400

    Garanteed that we could execute the load more than once.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -16,7 +16,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         /* To set the url you could you do it through Dependency injection or Singleton
          Dependency injection: constructor, property or method injection.
          But there's no need for a singleton, cause it creates tight coupling,
@@ -29,6 +29,20 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func test_load_requestsDataFromURLTwice() {
+        /* To set the url you could you do it through Dependency injection or Singleton
+         Dependency injection: constructor, property or method injection.
+         But there's no need for a singleton, cause it creates tight coupling,
+         and there could be more than one API client.
+         */
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        sut.load()
+        sut.load()
+         
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -39,9 +53,11 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
