288905530cf51a7d1c172cd854cd0f659507c8f3
neddyKG <gonzalezneddy@gmail.com>
2023-08-25 11:08:35 -0400

    Move RemoteFeedLoader to production

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,25 +6,7 @@
 //
 
 import XCTest
-@testable import EssentialFeed
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-    
-    init(url: URL , client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import EssentialFeed
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
@@ -35,9 +17,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     func test_load_requestDataFromURL() {
-        /* To set the url you could you do it throug Dependency injection or Singleton
+        /* To set the url you could you do it through Dependency injection or Singleton
          Dependency injection: constructor, property or method injection.
-         But there's no need for a singleton, cause there could be more than one API client.
+         But there's no need for a singleton, cause it creates tight coupling,
+         and there could be more than one API client.
          */
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
