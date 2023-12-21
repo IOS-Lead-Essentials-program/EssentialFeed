7ba5dd71960f0801e49a5f3161e94209e4f778ad
neddyKG <gonzalezneddy@gmail.com>
2023-12-21 18:43:20 -0400

    Remove references of 'items' in favor of 'images' which is a domain term used by domain experts in specs

--- EssentialFeed/Feed Cache/LocalFeedImage.swift
+++ EssentialFeed/Feed Cache/LocalFeedImage.swift
@@ -10,16 +10,16 @@ import Foundation
 /* Data Transfer Objects (DTO's). It looks like duplicate, but we allow them (LocalFeedItem vs. FeedItem) to change at their own pace.
  This removes strong coupling between modules.
  */
-public struct LocalFeedItem: Equatable {
+public struct LocalFeedImage: Equatable {
     public let id: UUID
     public let description: String?
     public let location: String?
-    public let imageURL: URL
+    public let url: URL
     
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+    public init(id: UUID, description: String?, location: String?, url: URL) {
         self.id = id
         self.description = description
         self.location = location
-        self.imageURL = imageURL
+        self.url = url
     }
 }
